2dc3607e34c5666a738702029486377416b39c52
chinnu <chinnu1@gmail.com>
2019-01-13 19:50:40 +0100

    first commit

---
+++ Tester/src/main/java/com/initial/EntryClass.java
@@ -0,0 +1,17 @@
+package com.initial;
+
+public class EntryClass {
+
+    public static void main(String...args){
+        System.out.println("This is the entry class");
+        Student student=generateStudentDetails();
+        System.out.println("Student Name : "+student.getName()+"\n RollNo : "+student
+        .getRollNo());
+    }
+    public static Student generateStudentDetails(){
+        Student student =new Student();
+        student.setName("John");
+        student.setRollNo(10);
+        return student;
+    }
+}
---
+++ Tester/src/main/java/com/initial/Student.java
@@ -0,0 +1,23 @@
+package com.initial;
+
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    private int rollNo;
+}
